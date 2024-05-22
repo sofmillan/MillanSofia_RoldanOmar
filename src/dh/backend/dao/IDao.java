9e668d49617e7c42bc89ec2da531d730e5174469
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-05-21 19:18:08 -0500

    First commit

---
+++ src/dh/backend/dao/IDao.java
@@ -0,0 +1,4 @@
+package dh.backend.dao;
+
+public class IDao {
+}
---
+++ src/dh/backend/dao/impl/OdontologoDaoH2.java
@@ -0,0 +1,10 @@
+package dh.backend.dao.impl;
+
+import dh.backend.db.H2Connection;
+
+import java.sql.*;
+
+public class OdontologoDaoH2 {
+
+
+}
